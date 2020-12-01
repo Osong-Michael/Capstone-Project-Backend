9bb1b61fdcee88795f59f86d10a453ec9ea7be60
Osong Agberndifor <agberndifor7@gmail.com>
2020-12-01 16:50:29 +0100

    Add data for seeding in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,58 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+Shoe.create!(name: 'Air Jordan 1', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814866/my-shoes/air-1_vio9gt.png', price: '$170', description: "Designed by Peter Moore, the Jordan 1 originally released from 1985 to 1986. Quite a few colorways of the model were produced, as well as a few different variations. Following its initial run in the 80s, the Jordan 1 jumpstarted the retro era in 1994, following Jordan's retirement from basketball.")
+
+Shoe.create!(name: 'Air Jordan 2', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814865/my-shoes/air-2_nauwhg.png', price: '$200', description: "Originally released in 1987, the Air Jordan 2 was designed by Bruce Kilgore. The sneakers were made in Italy, providing a premium feel to them. They released in two colorways, available in high and low top. In 1994 they were retroed for the first time, one colorway was released in high and low.")
+
+Shoe.create!(name: 'Air Jordan 3', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814866/my-shoes/air-3_hau9az.png', price: '$200', description: "The Air Jordan 3 is Michael Jordan's third signature basketball shoe and remains as one of the most sought after sneakers to-date. They were the first pair of Jordan sneakers that legendary designer Tinker Hatfield created, and they made their official debut in the year 1988")
+
+Shoe.create!(name: 'Air Jordan 4', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814866/my-shoes/air-4_cpp1ey.png', price: '$200', description: "In 1989, Nike released the Air Jordan IV to the public. Designed by Tinker Hatfield, it was the first Air Jordan released on the global market. It had four colorways: White/Black, Black/Cement Grey, White/Fire Red-Black, and Off White/Military Blue. Nike featured director and actor Spike Lee in ads for the AJ IV")
+
+Shoe.create!(name: 'Air Jordan 5', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814866/my-shoes/air-5_tt3tan.png', price: '$220', description: "The Air Jordan 5 was released in February 1990. The Air Jordan V was the first in the Air Jordan line to have a clear rubber sole. Tinker Hatfield also designed the Nike Marty MacFly’s featured in the movie Back to the Future II, they had clear molding which allowed light to come through, and that is were Tinker got the idea from.")
+
+
+Shoe.create!(name: 'Air Jordan 6', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814867/my-shoes/air-6_zvwjtl.png', price: '$190', description: "Originally released in 1991, Michael Jordan wore the 6 en route to his first NBA championship. The year 2000 saw the shoe retroed for the first time, with the introduction of new colorways and Jumpman logos replacing the original 'Nike Air' branding")
+
+
+Shoe.create!( name: 'Air Jordan 7', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814867/my-shoes/air-7_xsdzh2.png', price: '$200', description: "The Air Jordan 7 originally released in 1992 and was once again designed by Tinker Hatfield. Unlike the previous four models, the 7 did away with the visible Air unit in the heel and 'Nike Air' logo on the heel" )
+
+
+Shoe.create!( name: 'Air Jordan 8', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814867/my-shoes/air-8_qslr7r.png', price: '$140', description: "Originally released in 1993, the Air Jordan 8 features two cross-straps over the laces. Three colorways were released, Bulls 'Home,'' All-Star 'Aqua' and Bulls 'Playoff' editions, each packaged in a new Air Jordan box that was also used for the AJ 9, AJ 10 and first two colorways of the AJ 11" )
+
+
+Shoe.create!( name: 'Air Jordan 9', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814867/my-shoes/air-9_qemeso.png', price: '$190', description: "The Air Jordan 9 originally released in 1993-94 and was once again designed by Tinker Hatfield. It was the first model to be released when Michael Jordan retired from professional basketball to pursue a new career in baseball. They were not absent from the hardwood despite MJ being gone." ) 
+
+Shoe.create!( name: 'Air Jordan 10', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814868/my-shoes/air-10_d6smpx.png', price: '$220', description: "After his baseball-playing hiatus from the NBA, Michael Jordan returned to the game he was meant to play in March of 1995. With MJ not on the NBA court since the last game of the 1993 Finals, we missed out on him wearing the Air Jordan 9 entirely. But upon his return, it was the Air Jordan 10 in the Chicago colorway—complete with his new #45 stitched at the ankles—on the hardwood" )
+
+
+Shoe.create!( name: 'Air Jordan 11', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814867/my-shoes/air-11_ac9nb4.png', price: '$220', description: "The Air Jordan XI was originally released from 1995 to 1996. It was retro'd in 2000, 2001, 2003, 2006–2018 and 2019. The Air Jordan XI is the most popular Air Jordan in the series and has released almost ritualistically every December since 2008, selling out in minutes. It also is Hatfield's favorite." )
+
+
+Shoe.create!( name: 'Air Jordan 12', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814867/my-shoes/air-12_fq7nz5.png', price: '$190', description: "The Air Jordan XII was originally released from 1996 to 1997. It was retro-ed in 2003, 2004, 2008, 2009, 2011–2013, 2015–2018, and its most recent retro in 2019." )
+
+
+Shoe.create!( name: 'Air Jordan 13', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814867/my-shoes/air-13_ozfemx.png', price: '$190', description: "The Air Jordan 13 is Michael Jordan’s thirteenth signature basketball shoe with NIKE, Inc. Originally released in 1997 during “The Last Dance,” the sneaker was designed by the legendary Tinker Hatfield." )
+
+
+Shoe.create!( name: 'Air Jordan 14', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814868/my-shoes/air-14_ebhuyo.png', price: '$190', description: "Originally released from 1998-1999, the Jordan 14 was the first signature shoe to release after Michael Jordan retired from the NBA for the second time. Eight colorways were released, with three of them low top." )
+
+
+Shoe.create!( name: 'Air Jordan 15', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814868/my-shoes/air-15_gfsmvq.png', price: '$200', description: "The Air Jordan 15 debuted in 1999 as the first signature Michael Jordan sneaker to release after his second retirement. Designed by Tinker Hatfield, the Air Jordan 15 was inspired by the X-15 hypersonic jet plane." )
+
+
+Shoe.create!( name: 'Air Jordan 16', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814868/my-shoes/air-16_pphcim.png', price: '$160', description: "The Basics. The Air Jordan XVI released in 2001, debuting on Michael Jordan's birthday, February 17th. The shoe was released in four colorways retailing at $160 throughout the year, with an additional two low-top versions (that were actually almost the same height as the mid)" )
+
+
+Shoe.create!( name: 'Air Jordan 17', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814864/my-shoes/air-17_hglco8.png', price: '$200', description: "Designed by Wilson Smith III, the Air Jordan 17 made its debut in 2002 as Michael Jordan resigned from his President of Basketball Operations position with the Washington Wizards to return to the NBA once again. The sneaker retailed for $200 and retailed with a suitcase and a CD" )
+
+
+Shoe.create!( name: 'Air Jordan 18', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814866/my-shoes/air-18_ujp8sa.png', price: '$175', description: "Designed by Tate Kuerbis, the Air Jordan 18 released in 2003. The model, inspired by Italian dress shoes and foreign race cars, was Michael Jordan's primary game shoe during his second season with the Washington Wizards." )
+
+
+Shoe.create!( name: 'Air Jordan 19', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814866/my-shoes/air-19_dxbdte.png', price: '$200', description: "The Air Jordan 19 (XIX) was released in 2004. As Michael had retired Jordan Brand turned to players such as Jason Kidd, Gary Payton, Carmelo Anthony and of course Ray Allen, to promote the new Air Jordan. The lightest and most supportive Air Jordan sneaker to date featured an ultimate combination of sport and style." )
+
+
+Shoe.create!( name: 'Air Jordan 20', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814866/my-shoes/air-20_txs5zz.png', price: '$175', description: "The Air Jordan 20 would launch in 2005, which means it was Nike and Jordan Brand's 20th anniversary. Tinker Hatfield had not designed for Jordan Brand since the Air Jordan XV, but it was Gentry Humphries' (Footwear Business Director) idea to have him design the Air Jordan XX." )
+
+Shoe.create!( name: 'Air Jordan 21', image: 'https://res.cloudinary.com/dus4yypr8/image/upload/v1606814866/my-shoes/air-21_r6qrwq.png', price: '$200', description: "Jordan Brand went with designer D’Wayne Edwards for the Air Jordan 21. The design of the Air Jordan XXI is very simple compared to the other Air Jordan models. But like always, the Air Jordan XXI was full of high tech materials." )
