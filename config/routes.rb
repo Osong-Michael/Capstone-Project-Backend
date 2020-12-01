fee34a861c15243874353b77fa6edf72e00d1b5c
Osong Agberndifor <agberndifor7@gmail.com>
2020-12-01 14:41:22 +0100

    Set up backend authentication for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'shoes', to: 'shoes#index'
   get 'shoes/:id', to: 'shoes#show'
+  resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+  delete :logout, to: 'sessions#logout'
+  get :logged_in, to: 'sessions#logged_in'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
