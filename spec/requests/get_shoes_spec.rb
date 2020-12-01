dd0f7a7f64faf1e35a692e38ecb276eda9f53793
Osong Agberndifor <agberndifor7@gmail.com>
2020-12-01 16:31:27 +0100

    fix lint errors

--- spec/requests/get_shoes_spec.rb
+++ spec/requests/get_shoes_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe ShoesController, type: :controller do
-  describe "GET #index" do
+  describe 'GET #index' do
     before do
       get :index
     end
-    it "returns http success" do
+    it 'returns http success' do
       expect(response).to have_http_status(:success)
     end
-    it "JSON body response contains expected shoe attributes" do
+    it 'JSON body response contains expected shoe attributes' do
       expect(response.body).to match('[]')
     end
   end
