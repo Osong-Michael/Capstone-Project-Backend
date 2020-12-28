32a17aaccfedc8572b5f90457a093ceb2da0731b
Osong Agberndifor <agberndifor7@gmail.com>
2020-12-28 10:49:35 +0100

    Changes cors setup

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == 'production'
-  Rails.application.config.session_store :cookie_store, key: "_my_api", domain: 'https://vast-earth-24958.herokuapp.com'
+  Rails.application.config.session_store :cookie_store, key: "_my_api", domain: 'vast-earth-24958.herokuapp.com'
 else
   Rails.application.config.session_store :cookie_store, key: "_my_api"
 end
